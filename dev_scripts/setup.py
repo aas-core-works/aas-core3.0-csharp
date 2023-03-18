83a1b93b1a9372c6aaafba070ade0ec055fd6310
Marko Ristin <marko.ristin@gmail.com>
2023-03-18 13:32:39 +0100

    Decouple Python SDK from its repository
    Originally, we included the Python SDK in the requirements. However,
    this tightly couples C# SDK with Python SDK so we cannot update either
    of the repositories to different versions of aas-core-meta.
    
    We copy now the Python code directly from aas-core-codegen, and decouple
    thus this repository from aas-core3.0-python. This allows us now to
    atomically update *this repository* to the latest aas-core-meta without
    touching aas-core3.0-python.

--- dev_scripts/setup.py
+++ dev_scripts/setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 setup(
     name="aas-core3.0-csharp-dev-scripts",
     version="0.0.1",
-    description="Provide scripts for development of aas-core3.0-csharp.",
+    description="Provide development scripts for aas-core3.0-csharp.",
     url="https://github.com/aas-core-works/aas-core3.0-csharp",
     author="Marko Ristin",
     author_email="marko@ristin.ch",
@@ -26,11 +26,10 @@ setup(
     ],
     license="License :: OSI Approved :: MIT License",
     keywords="asset administration shell code generation industry 4.0 industrie i4.0",
-    packages=find_packages(exclude=["tests", "continuous_integration"]),
+    packages=find_packages(exclude=["tests", "continuous_integration", "dev_scripts"]),
     install_requires=[
         "aas-core-meta@git+https://github.com/aas-core-works/aas-core-meta@9ccd31e#egg=aas-core-meta",
         "aas-core-codegen@git+https://github.com/aas-core-works/aas-core-codegen@fcc252c#egg=aas-core-codegen",
-        "aas-core3.0@git+https://github.com/aas-core-works/aas-core3.0-python@e232aa2#egg=aas-core3.0",
     ],
     py_modules=["test_codegen"],
 )
