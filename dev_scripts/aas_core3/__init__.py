09f3862bda1c577b06571d3f6a265e42b367a4a2
Marko Ristin <marko@ristin.ch>
2023-09-08 12:09:55 +0200

    Update to aas-core-meta, codegen, testgen 4d7e59e, f3d9538, 9b43de2e (#22)
    We update the development requirements to and re-generate everything
    with:
    * [aas-core-meta 4d7e59e],
    * [aas-core-codegen f3d9538] and
    * [aas-core3.0-testgen 9b43de2e].
    
    Notably, this includes two important fixes propagated from
    aas-core-meta:
    
    * [V3 Fix AASc-3a-010-for-NAND (#281)], and
    * [V3 Fix inclusive OR in AASd-131 (#280)].
    
    ... and the following patch in aas-core-codegen:
    
    * [Make visitation dispatches virtual in C# (#399)]
    
    [aas-core-meta 4d7e59e]: https://github.com/aas-core-works/aas-core-meta/commit/4d7e59e
    [aas-core-codegen f3d9538]: https://github.com/aas-core-works/aas-core-codegen/commit/f3d9538
    [aas-core3.0-testgen 9b43de2e]: https://github.com/aas-core-works/aas-core3.0-testgen/commit/9b43de2e
    [V3 Fix AASc-3a-010-for-NAND (#281)]: https://github.com/aas-core-works/aas-core-meta/pull/281
    [V3 Fix inclusive OR in AASd-131 (#280)]: https://github.com/aas-core-works/aas-core-meta/pull/280
    [Make visitation dispatches virtual in C# (#399)]: https://github.com/aas-core-works/aas-core-codegen/pull/399

--- dev_scripts/aas_core3/__init__.py
+++ dev_scripts/aas_core3/__init__.py
@@ -3,5 +3,5 @@ Provide Python SDK as copied from aas-core-codegen test data.
 
 This copy is necessary so that we can decouple from ``aas-core*-python`` repository.
 
-The revision of aas-core-codegen was: e2b793f
+The revision of aas-core-codegen was: f3d9538
 """
