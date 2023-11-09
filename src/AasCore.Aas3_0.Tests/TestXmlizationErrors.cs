8c7d70339167ee35415762afeebd057776ef07b9
Marko Ristin <marko.ristin@gmail.com>
2023-11-09 11:50:26 +0100

    Fix error message in xmlization on XML declaration
    When we encountered XML declarations, we threw exceptions with
    uninformative messages. Namely, we expect the reader to be moved to the
    content, but most users omitted to read that in the documentation.
    
    We refine the error message for this particular situation, and hint at
    `MoveToContent` method on the reader.
    
    Fixes #24.
    
    Generated with [aas-core-codegen 28fbd98].
    
    [aas-core-codegen 28fbd98]: https://github.com/aas-core-works/aas-core-codegen/commit/28fbd98

---
+++ src/AasCore.Aas3_0.Tests/TestXmlizationErrors.cs
@@ -0,0 +1,55 @@
+﻿using Aas = AasCore.Aas3_0; // renamed
+
+using NUnit.Framework; // can't alias
+
+namespace AasCore.Aas3_0.Tests
+{
+    public class TestXmlizationErrors
+    {
+        [Test]
+        public void Test_error_on_unexpected_declaration()
+        {
+            var text = (
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                "<environment xmlns=\"https://admin-shell.io/aas/3/0\">" +
+                "<submodels><submodel>" +
+                "<id>some-unique-global-identifier</id>" +
+                "<submodelElements><property><idShort>someProperty</idShort>" +
+                "<valueType>xs:boolean</valueType></property></submodelElements>" +
+                "</submodel></submodels></environment>"
+            );
+
+            using var stringReader = new System.IO.StringReader(
+                text);
+
+            using var xmlReader = System.Xml.XmlReader.Create(
+                stringReader);
+
+            // We intentionally do not call `MoveToContent` to test the error message.
+            // This is a very common situation, see:
+            // https://github.com/aas-core-works/aas-core3.0-csharp/issues/24
+
+            string? message = null;
+
+            try
+            {
+                Aas.Xmlization.Deserialize.EnvironmentFrom(
+                    xmlReader);
+            }
+            catch (AasCore.Aas3_0.Xmlization.Exception exception)
+            {
+                message = exception.Message;
+            }
+
+            if (message == null)
+            {
+                throw new AssertionException("Unexpected no exception");
+            }
+            Assert.AreEqual(
+                "Unexpected XML declaration when reading an instance of " +
+                "class Environment, as we expect the reader to be set at content " +
+                "with MoveToContent at: the beginning",
+                message);
+        }
+    }
+}
