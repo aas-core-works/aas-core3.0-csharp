d4b873478489da74763d6df304056cd2c6e6bb76
Marko Ristin <marko.ristin@gmail.com>
2024-03-23 11:30:01 +0100

    Update to aas-core-meta, codegen, testgen cb28d18, c414f32, 6ff39c260
    We update the development requirements to and re-generate everything
    with:
    * [aas-core-meta cb28d18],
    * [aas-core-codegen c414f32] and
    * [aas-core3.0-testgen 6ff39c260].
    
    [aas-core-meta cb28d18]: https://github.com/aas-core-works/aas-core-meta/commit/cb28d18
    [aas-core-codegen c414f32]: https://github.com/aas-core-works/aas-core-codegen/commit/c414f32
    [aas-core3.0-testgen 6ff39c260]: https://github.com/aas-core-works/aas-core3.0-testgen/commit/6ff39c260
    
    We propagate [the fix from abnf-to-regex] related to maximum qualifiers
    which had been mistakenly represented as exact repetition before.
    
    [the fix from abnf-to-regex]: https://github.com/aas-core-works/abnf-to-regexp/pull/34

--- dev_scripts/aas_core3/__init__.py
+++ dev_scripts/aas_core3/__init__.py
@@ -3,5 +3,5 @@ Provide Python SDK as copied from aas-core-codegen test data.
 
 This copy is necessary so that we can decouple from ``aas-core*-python`` repository.
 
-The revision of aas-core-codegen was: 94399e1
+The revision of aas-core-codegen was: c414f32
 """
